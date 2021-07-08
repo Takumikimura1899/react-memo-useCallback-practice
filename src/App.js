44083c445870260393e4e06bc90960d3dc796e34
Takumikimura1899 <81891074+Takumikimura1899@users.noreply.github.com>
2021-07-09 05:18:01 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  // memoの変数版　あまり使うことはない。　変数の中が複雑化したときに再レンダリングされない。
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
